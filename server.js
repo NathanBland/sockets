53680c2929fb3a89c50ca414eaf9426c66addc1d
Nathan Bland <nathan.bland@gmail.com>
2015-02-27 02:37:56 +0000

    Pushing working chat service

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(8080);
+server.listen(process.env.PORT || 8000);
+console.log(process.env.PORT)
 
-app.get('/', function(req,res,next){
+app.get('/', function(req, res, next) {
     res.sendfile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-    socket.broadcast.emit('news', { user: 'connected' });
-    socket.on('my other event', function(data){
-        console.log(data);
+var users = 0;
+io.on('connection', function(socket) {
+    users += 1;
+    console.log("client connected");
+    socket.broadcast.emit('news', {
+        user: 'connected: ' + users
+    });
+    socket.on('disconnect', function() {
+        users -= 1;
+        console.log("client disconnected");
+        socket.broadcast.emit('news', {
+            user: 'disconnected'
+        });
+    });
+    socket.on('message', function(message) {
+        console.log(message);
+        socket.broadcast.emit('message', message.data);
     });
 });
